250fe208a35535585a3754d26d2df927f4d21e7b
JamesMcGuigan <james.mcguigan@gmail.com>
2015-01-27 06:43:00 +0000

    Updated SSL generation scripts

--- sslcert/san/generate-san.sh
+++ sslcert/san/generate-san.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 # @link http://techbrahmana.blogspot.co.uk/2013/10/creating-wildcard-self-signed.html
 
-# NOTE: Copies of infographic.san.key/infographic.san.crt have been copied to /puppet/modules/sslcerts/files
+# NOTE: Copies of statistical-learning.san.key/statistical-learning.san.crt have been copied to /puppet/modules/sslcerts/files
 
 cd    "$(dirname "$0")"
 
@@ -11,18 +11,18 @@ State=London
 City=London
 Organization="Crystalline Technologies"
 Section=""
-FQDN=infographic.jamesmcguigan.com
+FQDN=statistical-learning.jamesmcguigan.com
 Email=james.mcguigan@gmail.com
 
 
 ## Generate Private Key
-openssl genrsa -des3 -passout pass:foobar -out infographic.san.key.password 2048
+openssl genrsa -des3 -passout pass:foobar -out statistical-learning.san.key.password 2048
 
 ##  Convert the private key to an unencrypted format
-openssl rsa -passin pass:foobar -in infographic.san.key.password -out infographic.san.key
+openssl rsa -passin pass:foobar -in statistical-learning.san.key.password -out statistical-learning.san.key
 
 ##  Create the certificate signing request
-openssl req -new -key infographic.san.key -out infographic.san.csr <<EOF
+openssl req -new -key statistical-learning.san.key -out statistical-learning.san.csr <<EOF
 $Country
 $State
 $City
@@ -35,13 +35,13 @@ $Email
 EOF
 
 ## Sign the certificate with extensions
-openssl x509 -req -extensions v3_req -days 365 -in infographic.san.csr -signkey infographic.san.key -out infographic.san.crt -extfile infographic.san.conf
-#    -CA ../rootCA/infographic.rootCA.crt -CAkey ../rootCA/infographic.rootCA.key -CAcreateserial
+openssl x509 -req -extensions v3_req -days 365 -in statistical-learning.san.csr -signkey statistical-learning.san.key -out statistical-learning.san.crt -extfile generate.san.conf
+#    -CA ../rootCA/statistical-learning.rootCA.crt -CAkey ../rootCA/statistical-learning.rootCA.key -CAcreateserial
 
 #
-#openssl genrsa             -out infographic.san.key 2048
-#openssl req    -new -nodes -out infographic.san.csr -config infographic.san.conf
-#openssl x509   -req -CA ../rootCA/infographic.rootCA.pem -CAkey ../rootCA/infographic.rootCA.key -CAcreateserial -in infographic.san.csr -out infographic.san.crt -days 3650
+#openssl genrsa             -out statistical-learning.san.key 2048
+#openssl req    -new -nodes -out statistical-learning.san.csr -config generate.san.conf
+#openssl x509   -req -CA ../rootCA/statistical-learning.rootCA.pem -CAkey ../rootCA/statistical-learning.rootCA.key -CAcreateserial -in statistical-learning.san.csr -out statistical-learning.san.crt -days 3650
 ##end
 
 exit 0
